cd0bf8a11c5cd7c444a770d33b28b8de35fb91e5
Winston Saunders <winstonasaunders@gmail.com>
2015-04-13 07:25:50 -0700

    initial load

---
+++ explore1.R
@@ -0,0 +1,9 @@
+## PDX DATA SCIENCE KAGGLE PROJECT
+##
+
+## Get trining data
+##
+        directory<- "/Users/winstonsaunders/Documents/pdxkagglegroupproductclassproject/"
+        file_name<- "train.csv"
+        
+        train_data<-read.csv(paste0(directory,file_name))
