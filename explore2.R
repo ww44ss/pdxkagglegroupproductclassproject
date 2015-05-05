b67c1a9016c9a37629c0ab00def8dead1c70d9d8
Winston Saunders <winstonasaunders@gmail.com>
2015-05-06 00:13:01 +0200

    move to explore 2 - big improvement!

--- explore2.R
+++ explore2.R
@@ -22,33 +22,26 @@
 ## PRINT THE DIMENSIONS OF THE TRAINING DATA
         ## ensure proper read of data
         
-        print(dim(train_data))
+        cat(dim(train_data))
    
 ## SAMPLE DATA FOR QUICK LOOKS
         ##
         ## if sample_data == TRUE then creates a sample of overall data
         sample_data<-TRUE
         
-        set.seed(8675309)    
+        set.seed(8675309)
         if (sample_data == TRUE){
-                feats<-colnames(train_data)
-                feats<-feats[3:93]
-        
-                        sample_rows <- sample(1:dim(train_data)[1], 2*dim(train_data)[1]/3)
-                        #sample_columns<-c("id","feat_1", sample(feats, 28), "feat_93", "target")
-        
-                        train_data_T <- train_data[sample_rows, ] #sample_columns]
-                        
-                        test_data<-train_data[-sample_rows,]
-                        dim(train_data_T)
-        
-                        ## assign subset to data frame name temporarily
-        
-                        train_data<-train_data_T ## remove this for full analysis
-        }    
-        
+                sample_rows <- sample(1:dim(train_data)[1], 2*dim(train_data)[1]/3)
+                training<-train_data[sample_rows,]
+                testing<-train_data[-sample_rows,]
+                
+                train_data<-training
+                test_data<-testing
+        }
+
 ## DATA MUNGING
         ## convert from wide to long format
+        ## useful for plotting etc.
         require(tidyr)
         ## use tidyr package to munge the data into a long format with the feature id as a feature variable
         long_train<-gather(train_data, feature, data, feat_1:feat_93)
@@ -106,33 +99,21 @@
         
         ## can see quite a difference
         
-## TREE MODEL
+## TREE MODELS
+## Let's start predicting
         
-## USE {rpart} PACKAGE
-        
-        library(rpart)
-        ###
-        fit<-rpart(target~.-target-id, method="class", 
-                   data=train_data)
-        
-        plot(fit)
-        
-
-        plot(fit, uniform=TRUE, 
-             main="Classification Tree for Product Class")
-        text(fit, use.n=TRUE, all=TRUE, cex=.5)  
 
 ## USE {tree} PACKAGE
         
         library(tree)
         
+        #train_control<-tree.control(nobs=dim(train_data)[1], mindev=0.01/2)
         train_control<-tree.control(nobs=dim(train_data)[1], mindev=0.01/2)
-        
         ## renumber rows
         row.names(train_data) <- 1:nrow(train_data)
         
         tree_fit<-tree(target~.-id, method="class", 
-                       data=train_data, control = train_control)
+                       data=train_data)#, control = train_control)
         
         ## summary of the tree
         
@@ -143,41 +124,76 @@
         text(tree_fit, pretty=0)
         
         ## make sure the test data is there 
-        #head(test_data)
+        # head(test_data)
 
-        row.names(test_data) <- 1:nrow(test_data)
-        
         td<-test_data
-        td$id<-NULL
   
-        
-        td<-td[1:10,]
-        
-        predicted_classification<-predict(tree_fit, td, type="class")
-        
-        
-        table(predicted_classification)
-        table(test_data$target)
-        
-        table(predicted_classification, test_data$id)
-        
-        length(test_data$target)
-        length(predicted_classification)
-        
-        
-        ## shows substantial error
-        
-## look at CV Tree for pruning
-        
-        set.seed(8675309)
-        cv_tree_fit <- cv.tree(tree_fit, FUN=prune.misclass)
-        
-        names(cv_tree_fit)
-        
-        plot(cv_tree_fit$size, cv_tree_fit$dev, type="b")
-        
- 
-## Let's check the confusion matrix
+        predicted_classification<-predict(tree_fit, newdata=td, type="class")
         
         table(predicted_classification, test_data$target)
+        
+        check<-table(predicted_classification ==test_data$target)
+        cat(check)
+        
+        accuracy<-1-check[1]/(check[1]+check[2])
+        
+        cat(accuracy)   ## 58%
+        
+        ## not verry good. 
+        ## missing several rows (Class_1, 3, 4, 7)
+        
+## TRY TWO SEPARATE TREES
+        
+        train<-train_data
+        train_a<-train[train$target=="Class_2"|train$target=="Class_5"|train$target=="Class_6"|train$target=="Class_8"|train$target=="Class_9",]
+        train_b<-train[train$target=="Class_1"|train$target=="Class_3"|train$target=="Class_4"|train$target=="Class_7",]
+        
+        tree_a<-tree(target~.-id, method="class", 
+                       data=train_a)
+        tree_b<-tree(target~.-id, method="class", data=train_b)
+        
+        
+        plot(tree_b)
+        text(tree_b, pretty=0)
+        
+        test<-test_data
+        test_a<-test[test$target=="Class_2"|test$target=="Class_5"|test$target=="Class_6"|test$target=="Class_8"|test$target=="Class_9",]
+        test_b<-test[test$target=="Class_1"|test$target=="Class_3"|test$target=="Class_4"|test$target=="Class_7",]
+        
+        
+        predict_a<-predict(tree_a, newdata=test_a, type="class")
+        predict_b<-predict(tree_b, newdata=test_b, type="class")
+        
+        head(predict_a)
+        head(predict_b)
+        
+        table(test_a$target, predict_a)
+        table(test_b$target, predict_b)
+        
+        ## put everything back together 
+        ## this is a little convoluted
+        
+        result_a<-cbind(id=test_a$id, target=as.data.frame(predict_a))
+        colnames(result_a)<-c("id", "target")
+        result_b<-cbind(test_b$id, as.data.frame(predict_b))
+        colnames(result_b)<-c("id", "target")
+        result<-rbind(result_a, result_b)
+        result<-result[order(result[,1]),]
+        
+        table(test_data$target, result$target)
+        
+        check<-table(test_data$target==result$target)
+        cat(check)
+        
+        accuracy<-1-check[1]/(check[1]+check[2])
+        
+        cat(accuracy)   ## 78%
+        
+        ## a big improvement
+        
+        
+        
+        
+        
+        
         
