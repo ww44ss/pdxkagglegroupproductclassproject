a1e4409825aba36cf304a832e3331e8f478b202e
Winston Saunders <winstonasaunders@gmail.com>
2015-05-07 00:31:56 +0200

    making more trials of gbm

--- explore2.R
+++ explore2.R
@@ -198,7 +198,222 @@
         
         
         
+### TRY GBM2
+        ## USE {gbm} PACKAGE
         
+        ## from base above increase number of trees and reduce shrinkage by facgor of two.
+        
+        library(gbm)
+        
+        #train_control<-tree.control(nobs=dim(train_data)[1], mindev=0.01/2)
+        #train_control<-tree.control(nobs=dim(train_data)[1], mindev=0.01/2)
+        ## renumber rows
+        train<-train_data
+        
+        set.seed(8675309)
+        gbm_fit<-gbm(target~.-id, data=train, 
+                     n.trees = 200,
+                     distribution = "multinomial",
+                     shrinkage=0.025,
+                     interaction.depth=2,       ## interaction depth of 2 (normally between 1 and 3)
+                     train.fraction=0.5,
+                     keep.data=TRUE,
+                     verbose=TRUE,
+                     cv.folds=3,                ## 3-fold cv 
+                     n.cores=1)                 ## avoid annoying bugs
+        
+        ## summary of the tree
+        
+        fitsum <- summary(gbm_fit)
+        plot(fitsum)
+        
+        plot(gbm_fit, i.var=11)
+        ## plot and label
+        names(gbm_fit)
+        
+        gbm_plot<-as.data.frame(cbind(gbm_fit$train.error, gbm_fit$valid.error))
+        colnames(gbm_plot)<-c("train.error", "valid.error")
+        gbm_plot$iteration<-1:gbm_fit$n.tree
+        
+        
+        p<-ggplot(gbm_plot, aes(x=iteration, y = train.error))+geom_line()
+        p<-p+geom_line(aes(x=iteration, y =valid.error), color="red")
+        
+        print(p)
+        # can see a slight divergence of training adn test
+        
+        ## make sure the test data is there 
+        # head(test_data)
+        
+        td<-test_data
+        
+        td_model<-predict(gbm_fit, newdata=td, type="response")
+        
+        td_names<-colnames(td_model)
+        td_model<-as.data.frame(td_model)
+        colnames(td_model)<-td_names
+        td_predict <- as.factor(colnames(td_model)[max.col(td_model)])
+        
+        table(td_predict, td$target)
+        
+        check<-table(td_predict ==test_data$target)
+        cat(check)
+        
+        accuracy<-1-check[1]/(check[1]+check[2])
+        
+        cat(accuracy)   ## 73%
+        ## a significant improvement over tree 
+        
+### TRY GBM3
+        ## USE {gbm} PACKAGE
+        
+        ## from base above increase number of trees by factor of two and restore shrinkage .
+        
+        library(gbm)
+        
+        #train_control<-tree.control(nobs=dim(train_data)[1], mindev=0.01/2)
+        #train_control<-tree.control(nobs=dim(train_data)[1], mindev=0.01/2)
+        ## renumber rows
+        train<-train_data
+        
+        set.seed(8675309)
+        gbm_fit<-gbm(target~.-id, data=train, 
+                     n.trees = 200,
+                     distribution = "multinomial",
+                     shrinkage=0.05,
+                     interaction.depth=2,       ## interaction depth of 2 (normally between 1 and 3)
+                     train.fraction=0.5,
+                     keep.data=TRUE,
+                     verbose=TRUE,
+                     cv.folds=3,                ## 3-fold cv 
+                     n.cores=1)                 ## avoid annoying bugs
+        
+        ## summary of the tree
+        
+        fitsum <- summary(gbm_fit)
+        plot(fitsum)
+        
+        plot(gbm_fit, i.var=11)
+        ## plot and label
+        names(gbm_fit)
+        
+        gbm_plot<-as.data.frame(cbind(gbm_fit$train.error, gbm_fit$valid.error))
+        colnames(gbm_plot)<-c("train.error", "valid.error")
+        gbm_plot$iteration<-1:gbm_fit$n.tree
+        
+        
+        p<-ggplot(gbm_plot, aes(x=iteration, y = train.error))+geom_line()
+        p<-p+geom_line(aes(x=iteration, y =valid.error), color="red")
+        
+        print(p)
+        # can see a slight divergence of training adn test
+        
+        ## make sure the test data is there 
+        # head(test_data)
+        
+        td<-test_data
+        
+        td_model<-predict(gbm_fit, newdata=td, type="response")
+        
+        td_names<-colnames(td_model)
+        td_model<-as.data.frame(td_model)
+        colnames(td_model)<-td_names
+        td_predict <- as.factor(colnames(td_model)[max.col(td_model)])
+        
+        table(td_predict, td$target)
+        
+        
+        check<-table(td_predict ==test_data$target)
+        cat(check)
+        
+        accuracy<-1-check[1]/(check[1]+check[2])
+        
+        cat(accuracy)   ## 75.46%
+        ## significant misclassification of class_2
+        
+#         td_predict Class_1 Class_2 Class_3 Class_4 Class_5 Class_6 Class_7 Class_8 Class_9
+#         Class_1     237       2       5       0       0      21      24      45      51
+#         Class_2      81    4598    1695     501      35      91     185      85     106
+#         Class_3       7     568     912     107       4      17      69      15       3
+#         Class_4       0      21      20     219       0       1       6       0       2
+#         Class_5       2      16       1       7     885       1       2       3       3
+#         Class_6      50      15       3      36       1    4386      65      74      78
+#         Class_7      27      47      60      19       0      50     524      26       9
+#         Class_8     116      11      13       4       0      97      65    2452      98
+#         Class_9     140       9       7       2       0      72       6      59    1352
+
+        
+        ## a significant improvement over tree 
+        
+        
+### TRY GBM4
+        ## USE {gbm} PACKAGE
+        
+        ## from base above increase number of trees to 400 of two and keep shrinkage = 0.05
+        ## also decrease depth to 1
+        
+        library(gbm)
+        
+        #train_control<-tree.control(nobs=dim(train_data)[1], mindev=0.01/2)
+        #train_control<-tree.control(nobs=dim(train_data)[1], mindev=0.01/2)
+        ## renumber rows
+        train<-train_data
+        
+        set.seed(8675309)
+        gbm_fit<-gbm(target~.-id, data=train, 
+                     n.trees = 400,
+                     distribution = "multinomial",
+                     shrinkage=0.05,
+                     interaction.depth=1,       ## interaction depth of 2 (normally between 1 and 3)
+                     train.fraction=0.5,
+                     keep.data=TRUE,
+                     verbose=TRUE,
+                     cv.folds=3,                ## 3-fold cv 
+                     n.cores=1)                 ## avoid annoying bugs
+        
+        ## summary of the tree
+        
+        fitsum <- summary(gbm_fit)
+        plot(fitsum)
+        
+        plot(gbm_fit, i.var=11)
+        ## plot and label
+        names(gbm_fit)
+        
+        gbm_plot<-as.data.frame(cbind(gbm_fit$train.error, gbm_fit$valid.error))
+        colnames(gbm_plot)<-c("train.error", "valid.error")
+        gbm_plot$iteration<-1:gbm_fit$n.tree
+        
+        
+        p<-ggplot(gbm_plot, aes(x=iteration, y = train.error))+geom_line()
+        p<-p+geom_line(aes(x=iteration, y =valid.error), color="red")
+        
+        print(p)
+        # can see a slight divergence of training adn test
+        
+        ## make sure the test data is there 
+        # head(test_data)
+        
+        td<-test_data
+        
+        td_model<-predict(gbm_fit, newdata=td, type="response")
+        
+        td_names<-colnames(td_model)
+        td_model<-as.data.frame(td_model)
+        colnames(td_model)<-td_names
+        td_predict <- as.factor(colnames(td_model)[max.col(td_model)])
+        
+        table(td_predict, td$target)
+        
+        
+        check<-table(td_predict ==test_data$target)
+        cat(check)
+        
+        accuracy<-1-check[1]/(check[1]+check[2])
+        
+        cat(accuracy)   ## 75.46%
+        
+        ## a significant improvement over tree 
         
         
         
