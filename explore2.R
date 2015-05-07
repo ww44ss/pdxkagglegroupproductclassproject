3ef84ad175da0ee95690c7c22b2e2755ed8cb110
Winston Saunders <winstonasaunders@gmail.com>
2015-05-07 23:01:11 +0200

    big combo run

--- explore2.R
+++ explore2.R
@@ -598,7 +598,7 @@
         ## a small improvement
         
         
-### TRY GBM7
+### TRY GBM7 with an added RF
         ## USE {gbm} PACKAGE
         
         ## increase number of trees to 2000 of two and keep shrinkage = 0.03
@@ -659,6 +659,8 @@
         td<-test_data
         
         td_model<-predict(gbm_fit, newdata=td, type="response")
+        td_gbm_predict<-td_model[,,1]
+        td_gbm_predict<-as.data.frame(td_gbm_predict)
         
         td_names<-colnames(td_model)
         td_model<-as.data.frame(td_model)
@@ -694,7 +696,36 @@
         
         td_rf_model <- randomForest(target~.-id, data=train, importance=TRUE, ntree=200, nodesize=10)
         
+        td_rf_predict<-predict(td_rf_model, newdata=td, type="prob")
         
+        
+        ## average the two models
+        average_predict<-(td_rf_predict+td_gbm_predict)/2
+        ## pick the max probability
+        average_predict_single <- as.factor(colnames(average_predict)[max.col(average_predict)])
+        
+        table(average_predict_single, td$target)
+        
+#         average_predict_single 
+#                 Class_1 Class_2 Class_3 Class_4 Class_5 Class_6 Class_7 Class_8 Class_9
+#         Class_1     224       0       1       0       0      13      21      28      19
+#         Class_2      48    4713    1608     460      29      70     174      56      65
+#         Class_3       2     478    1026     110       0       5      48       7       3
+#         Class_4       0      14      18     264       0       1       6       0       0
+#         Class_5       2      13       3       6     890       0       4       2       2
+#         Class_6      56      14       3      37       3    4437      69      86      71
+#         Class_7      19      38      39      15       0      50     536       9       9
+#         Class_8     145      11      14       2       0      96      80    2530      86
+#         Class_9     164       6       4       1       3      64       8      41    1447
+#        
+#       Note this still does not fix the class_2 class_3 mixup
+        
+        check<-table(average_predict_single ==td$target)
+        cat(check)
+        
+        accuracy<-1-check[1]/(check[1]+check[2])
+        
+        cat(accuracy)
         
 ##MAKE SUBMISSION
         
@@ -706,9 +737,13 @@
         ##just make sure its real
         print(head(real_test_data))
         ## run the prediction
-        real_test_model<-predict(gbm_fit, newdata=real_test_data, type="response")
+        gbm_real_test_model<-predict(gbm_fit, newdata=real_test_data, type="response")
+        rf_real_test_model<-predict(td_rf_model, newdata=real_test_data, type="prob")
         ## get the data
-        submission<-real_test_model[,,1]
+        a<-gbm_real_test_model[,,1]
+        a<-as.data.frame(a)
+        b<-rf_real_test_model
+        submission<-(a+b)/2
         submission<-as.data.frame(submission)
         ## clean up the numbers
         submission<-round(10000*submission,0)/10000.
@@ -723,8 +758,173 @@
         tail(submission)
         
         ## write csv
-        write.csv(submission, paste0(directory,"May073",".csv"), row.names=F, quote=F)
+        write.csv(submission, paste0(directory,"May074",".csv"), row.names=F, quote=F)
         
+        
+#############################################
+### TRY GBM8 with an added RF
+        ## USE {gbm} PACKAGE
+        
+        ## increase number of trees to 2000 of two and inch down shrinkage = 0.025
+        ## keep depth to 2
+        ## reduce bag.fraction to 0.031 which will make 3
+        ## 
+        
+        library(gbm)
+        
+        
+        #train_control<-tree.control(nobs=dim(train_data)[1], mindev=0.01/2)
+        #train_control<-tree.control(nobs=dim(train_data)[1], mindev=0.01/2)
+        ## renumber rows
+        train<-train_data
+        
+        set.seed(8675309)
+        gbm_fit<-gbm(target~.-id, data=train, 
+                     n.trees = 2000,
+                     distribution = "multinomial",
+                     shrinkage=0.025,
+                     interaction.depth=2,       ## interaction depth of 1 (normally between 1 and 3)
+                     train.fraction=0.5,
+                     bag.fraction=0.031,
+                     keep.data=TRUE,
+                     verbose=TRUE,
+                     cv.folds=3,                ## 3-fold cv 
+                     n.cores=1)                 ## avoid annoying bugs
+        
+        
+        
+        
+        
+        
+        ## summary of the tree
+        
+        fitsum <- summary(gbm_fit)
+        plot(fitsum)
+        
+        plot(gbm_fit, i.var=11)
+        ## plot and label
+        #names(gbm_fit)
+        
+        gbm_plot<-as.data.frame(cbind(gbm_fit$train.error, gbm_fit$valid.error))
+        colnames(gbm_plot)<-c("train.error", "valid.error")
+        gbm_plot$iteration<-1:gbm_fit$n.tree
+        
+        
+        p<-ggplot(gbm_plot, aes(x=iteration, y = train.error))+geom_line()
+        p<-p+geom_line(aes(x=iteration, y =valid.error), color="red")
+        
+        print(p)
+        # can see a slight divergence of training adn test
+        ## still not over training
+        
+        ## make sure the test data is there 
+        # head(test_data)
+        
+        td<-test_data
+        
+        td_model<-predict(gbm_fit, newdata=td, type="response")
+        td_gbm_predict<-td_model[,,1]
+        td_gbm_predict<-as.data.frame(td_gbm_predict)
+        
+        td_names<-colnames(td_model)
+        td_model<-as.data.frame(td_model)
+        colnames(td_model)<-td_names
+        td_predict <- as.factor(colnames(td_model)[max.col(td_model)])
+        
+        table(td_predict, td$target)
+        
+        #         td_predict Class_1 Class_2 Class_3 Class_4 Class_5 Class_6 Class_7 Class_8 Class_9
+        #         Class_1     265       7       5       2       0      29      35      75      67
+        #         Class_2      45    4348    1408     352      29      53     112      38      60
+        #         Class_3       3     697    1135     143       1       3      70       8       3
+        #         Class_4       1     106      68     336       2      11      17       0       3
+        #         Class_5       3      23       6       5     884       0      11       4       5
+        #         Class_6      54      19       4      32       2    4363      82      76      73
+        #         Class_7      23      51      64      20       0      82     560      37      10
+        #         Class_8     114      21      18       2       6     102      51    2473      94
+        #         Class_9     152      15       8       3       1      93       8      48    1387
+        #         
+        check<-table(td_predict ==test_data$target)
+        cat(check)
+        
+        accuracy<-1-check[1]/(check[1]+check[2])
+        
+        cat(accuracy)   ## 76.37%
+        ## kaggle score 0.62881
+        
+        ## a small improvement 
+        
+        ## Add a Random Forest
+        
+        library(randomForest)
+        
+        td_rf_model <- randomForest(target~.-id, data=train, importance=TRUE, ntree=500, nodesize=5)
+        
+        td_rf_predict<-predict(td_rf_model, newdata=td, type="prob")
+        
+        
+        ## average the two models
+        average_predict<-(td_rf_predict+td_gbm_predict)/2
+        ## pick the max probability
+        average_predict_single <- as.factor(colnames(average_predict)[max.col(average_predict)])
+        
+        table(average_predict_single, td$target)
+        
+#average_predict_single Class_1 Class_2 Class_3 Class_4 Class_5 Class_6 Class_7 Class_8 Class_9
+#               Class_1     273       4       2       1       0      23      24      43      30
+#               Class_2      37    4566    1379     362      27      45     132      38      47
+#               Class_3       2     575    1220     129       1       4      60       8       2
+#               Class_4       0      49      41     348       1       4      14       0       1
+#               Class_5       1      11       2       6     889       0       5       2       3
+#               Class_6      47      17       5      30       3    4432      77      79      65
+#               Class_7      28      40      44      15       0      62     572      22       8
+#               Class_8     120      15      19       2       2      92      56    2535      81
+#               Class_9     152      10       4       2       2      74       6      32    1465
+       
+        #        
+        #       Note this still does not fix the class_2 class_3 mixup
+        
+        check<-table(average_predict_single ==td$target)
+        cat(check)
+        
+        accuracy<-1-check[1]/(check[1]+check[2])
+        
+        cat(accuracy)
+        
+        ## accuracy of 79.03%
+        
+        ##MAKE SUBMISSION
+        
+        ## get the actual test data
+        directory<- "/Users/winstonsaunders/Documents/pdxkagglegroupproductclassproject/"
+        file_name<- "test.csv"
+        
+        real_test_data<-read.csv(paste0(directory,file_name))
+        ##just make sure its real
+        print(head(real_test_data))
+        ## run the prediction
+        gbm_real_test_model<-predict(gbm_fit, newdata=real_test_data, type="response")
+        rf_real_test_model<-predict(td_rf_model, newdata=real_test_data, type="prob")
+        ## get the data
+        a<-gbm_real_test_model[,,1]
+        a<-as.data.frame(a)
+        b<-rf_real_test_model
+        submission<-(a+b)/2
+        submission<-as.data.frame(submission)
+        ## clean up the numbers
+        submission<-round(10000*submission,0)/10000.
+        
+        ## add ids back
+        submission<-cbind("id"=as.integer(real_test_data$id), submission)
+        submission<-as.data.frame(submission)
+        submission$id<-as.integer(submission$id)
+        options(scipen=10)
+        dim(submission)
+        head(submission)
+        tail(submission)
+        
+        ## write csv
+        write.csv(submission, paste0(directory,"May081",".csv"), row.names=F, quote=F)
         
         
         
